5158edd44d3bfcc468688c6952f746fadc0202ee
Richard Lupton <richard.lupton@gmail.com>
2018-01-29 00:11:34 +0000

    Comments

--- src/Main.hs
+++ src/Main.hs
@@ -7,18 +7,26 @@ import Data.List (foldl')
 -- as we go
 data Result a = Partial a String | Fail deriving (Eq, Show)
 
+-- A parser takes a string and returns a decision about how the parse
+-- is going
 newtype Parser a = Parser { runParser :: String -> Result a }
 
+-- Parsers have an obvious Functor instance
 instance Functor Parser where
   fmap f p = Parser $ \s -> case runParser p $ s of
     Partial x s -> Partial (f x) s
     Fail -> Fail
 
+-- A parse is successful if and only if we parsed something
+-- and have no input left to consume
 parse :: Parser a -> String -> Maybe a
 parse p s = case runParser p $ s of
   Partial a [] -> Just a
   _ -> Nothing
 
+-- If we can parse an a, and then parse a b, and have a way
+-- of combining a's and b's, we ought to be able to build a new
+-- parser which can parse our a and b, then provide us with a c
 joinWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
 joinWith f p q = Parser $ \s ->
   case runParser p $ s of
@@ -27,14 +35,18 @@ joinWith f p q = Parser $ \s ->
       Fail -> Fail
     _ -> Fail
 
+-- inject consumes no input, and returns a value
 inject :: a -> Parser a
 inject x = Parser $ \s -> Partial x s
 
+-- character parses out a single character from a string
 character :: Char -> Parser Char
 character c = Parser $ \s -> case s of
+  "" -> Fail
   c':s' -> if c == c' then Partial c s' else Fail
-  _ -> Fail
 
+-- tag parses a matching string of characters out of a String
+-- TODO make this efficient (the current implementation is crap)
 tag :: String -> Parser String
 tag =  let eatchars = joinWith $ flip (:) in
          fmap reverse . foldl' eatchars (inject "") . map character
@@ -49,9 +61,11 @@ chars c = Parser $ \s ->
 chars1 :: Char -> Parser String
 chars1 c = joinWith (:) (character c) (chars c)
 
+-- rest matches the rest of the input
 rest :: Parser String
 rest = Parser $ \s -> Partial s ""
 
+-- try one parser, and if that fails, try another
 orTry :: Parser a -> Parser b -> Parser (Either a b)
 orTry p q = Parser $ \s ->
   case runParser p $ s of
@@ -60,6 +74,8 @@ orTry p q = Parser $ \s ->
       Partial y s' -> Partial (Right y) s'
       Fail -> Fail
 
+-- like orTry, but for when both parsers provide the same type
+-- and we have no reason to keep them distinguished
 orTry' :: Parser a -> Parser a -> Parser a
 orTry' p q = fmap merge $ orTry p q
   where
